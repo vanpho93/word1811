56631796790c140879841cd8bb3741104c3ca15b
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-22 21:12:10 +0700

    shouldShowForm

--- src/App.js
+++ src/App.js
@@ -60,6 +60,40 @@ class App extends Component {
     );
   }
 
+  getForm() {
+    const { txtEn, txtVn, shouldShowForm } = this.state;
+    if (!shouldShowForm) return (
+      <button className="btn btn-success" onClick={() => this.setState({ shouldShowForm: true })}>
+        Create new word
+      </button>
+    );
+    return (
+      <div className="form-group" style={{ width: '200px' }}>
+        <input
+          placeholder="English"
+          className="form-control"
+          value={txtEn}
+          onChange={evt => this.setState({ txtEn: evt.target.value })}
+        />
+        <br />
+        <input
+          placeholder="Vietnamese"
+          className="form-control"
+          value={txtVn}
+          onChange={evt => this.setState({ txtVn: evt.target.value })}
+        />
+        <br />
+        <div className="btn-container">
+          <button className="btn btn-success" onClick={() => this.addWord()}>
+            Add word
+          </button>
+          <button className="btn btn-danger"  onClick={() => this.setState({ shouldShowForm: false })}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    );
+  }
   addWord() {
     const { txtEn, txtVn, words } = this.state;
     const word = {
@@ -69,38 +103,14 @@ class App extends Component {
       isMemorized: false
     };
     const newWords = [word, ...words];
-    this.setState({ words: newWords, txtEn: '', txtVn: '' });
+    this.setState({ words: newWords, txtEn: '', txtVn: '', shouldShowForm: false });
   }
 
   render() {
     const { txtEn, txtVn, words } = this.state;
     return (
       <div className="App container">
-        <button className="btn btn-success">Create new word</button>
-        <div className="form-group" style={{ width: '200px' }}>
-          <input
-            placeholder="English"
-            className="form-control"
-            value={txtEn}
-            onChange={evt => this.setState({ txtEn: evt.target.value })}
-          />
-          <br />
-          <input
-            placeholder="Vietnamese"
-            className="form-control"
-            value={txtVn}
-            onChange={evt => this.setState({ txtVn: evt.target.value })}
-          />
-          <br />
-          <div className="btn-container">
-            <button className="btn btn-success" onClick={() => this.addWord()}>
-              Add word
-            </button>
-            <button className="btn btn-danger">
-              Cancel
-            </button>
-          </div>
-        </div>
+        { this.getForm() }
         {words.map(word => this.getWordItem(word))}
       </div>
     );
