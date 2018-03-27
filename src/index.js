76fc253d4c55c32da24ee8d56a1f34104d53c1ef
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-27 20:14:51 +0700

    Provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootDiv = document.getElementById('root')
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootDiv);
 registerServiceWorker();
